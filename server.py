08a26705422c28b45b371d2fd62a1103e0068e50
adrian ramas rayego <aramas@iota26.aulas.gsyc.es>
2014-11-24 10:34:11 +0100

    servidor final sin pep8

--- server.py
+++ server.py
@@ -6,8 +6,12 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 
 import SocketServer
 import sys
+import os
 
 entrada = sys.argv
+if len(entrada) != 4:
+    print "Usage: python server.py IP port audio_file"
+    raise SystemExit
 IP = int(entrada[2])
 
 class EchoHandler(SocketServer.DatagramRequestHandler):
@@ -17,7 +21,6 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
 
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
-        self.wfile.write('Hemos recibido tu peticion\r\n\r\n')
         metodos = ["INVITE", "ACK", "BYE"]
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
@@ -29,14 +32,19 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
                 break
             print "El cliente nos manda " + line
             if metodo == "INVITE":
-                Mensaje = 'SIP/2.0 100 Trying\r\n\r\n' + 'SIP/2.0 180 Ring'
-                Mensaje += '\r\n\r\n' + 'SIP/2.0 200 OK\r\n\r\n'
+                Mensaje = 'SIP/2.0 100 Trying\r\n\r\n'
+		Mensaje += 'SIP/2.0 180 Ring\r\n\r\n'
+                Mensaje += 'SIP/2.0 200 OK\r\n\r\n'
                 print "Enviando: " + Mensaje
                 self.wfile.write(Mensaje)
             elif metodo == "BYE":
                 Mensaje = 'SIP/2.0 200 OK\r\n\r\n'
                 print "Enviando: " + Mensaje
                 self.wfile.write(Mensaje)
+            elif metodo == "ACK":
+                aEjecutar = './mp32rtp -i 127.0.0.1 -p 23032 < ' + entrada[3]
+                os.system('chmod 755 mp32rtp')
+                os.system(aEjecutar)
             elif metodo not in metodos:
                 Mensaje = 'SIP/2.0 405 Method Not Allowed\r\n\r\n'
                 self.wfile.write(Mensaje)
@@ -47,5 +55,5 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
     serv = SocketServer.UDPServer(("", IP), EchoHandler)
-    print "Lanzando servidor UDP de eco..."
+    print "Listening..."
     serv.serve_forever()
